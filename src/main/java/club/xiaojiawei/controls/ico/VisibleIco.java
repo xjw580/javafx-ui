9ed85560832b429b12bc83af916d184de6cd515d
zergqueen <xjw580@qq.com>
2023-12-01 18:03:07 +0800

    优化图标的宽度占用问题

--- src/main/java/club/xiaojiawei/controls/ico/VisibleIco.java
+++ src/main/java/club/xiaojiawei/controls/ico/VisibleIco.java
@@ -2,6 +2,7 @@ package club.xiaojiawei.controls.ico;
 
 import club.xiaojiawei.controls.images.ImagesLoader;
 import javafx.scene.layout.StackPane;
+import org.girod.javafx.svgimage.SVGImage;
 import org.girod.javafx.svgimage.SVGLoader;
 
 /**
@@ -11,6 +12,8 @@ import org.girod.javafx.svgimage.SVGLoader;
 public class VisibleIco extends StackPane {
 
     public VisibleIco() {
-        this.getChildren().add(SVGLoader.load(ImagesLoader.class.getResource(this.getClass().getSimpleName() + ".svg")));
+        SVGImage svgImage = SVGLoader.load(ImagesLoader.class.getResource(this.getClass().getSimpleName() + ".svg"));
+        this.setMaxWidth(svgImage.getWidth());
+        this.getChildren().add(svgImage);
     }
 }
