bcb4337e318350e348988ba709eca351551ea313
zergqueen <xjw580@qq.com>
2023-12-04 17:55:35 +0800

    增加图标：PersonIco、PlaybackIco、RecordIco、RecordingIco、ResistanceIco、ScreenshotIco

---
+++ src/main/java/club/xiaojiawei/controls/ico/PersonIco.java
@@ -0,0 +1,19 @@
+package club.xiaojiawei.controls.ico;
+
+import club.xiaojiawei.controls.images.ImagesLoader;
+import javafx.scene.layout.StackPane;
+import org.girod.javafx.svgimage.SVGImage;
+import org.girod.javafx.svgimage.SVGLoader;
+
+/**
+ * @author 肖嘉威 xjw580@qq.com
+ * @date 2023/10/31 22:28
+ */
+public class PersonIco extends StackPane {
+
+    public PersonIco() {
+        SVGImage svgImage = SVGLoader.load(ImagesLoader.class.getResource(this.getClass().getSimpleName() + ".svg"));
+        this.setMaxWidth(svgImage.getWidth());
+        this.getChildren().add(svgImage);
+    }
+}
