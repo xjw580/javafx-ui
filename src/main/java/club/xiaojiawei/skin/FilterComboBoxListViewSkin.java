265ce48d6f614278f1840227e73ce935c1b7c766
zergqueen <xjw580@qq.com>
2024-08-09 14:52:32 +0800

    v2.4.0
    1. 修改FilterComboBox实现方式

--- src/main/java/club/xiaojiawei/skin/FilterComboBoxListViewSkin.java
+++ src/main/java/club/xiaojiawei/skin/FilterComboBoxListViewSkin.java
@@ -3,24 +3,21 @@ package club.xiaojiawei.skin;
 import club.xiaojiawei.annotations.NotNull;
 import club.xiaojiawei.controls.FilterComboBox;
 import club.xiaojiawei.controls.FilterField;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
-import javafx.event.Event;
-import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.skin.ComboBoxListViewSkin;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
-import javafx.util.Callback;
+import javafx.scene.layout.VBox;
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author 肖嘉威 xjw580@qq.com
@@ -35,32 +32,36 @@ public class FilterComboBoxListViewSkin<T> extends ComboBoxListViewSkin<T> {
 
     private final StackPane filterPane = new StackPane(filterField);
 
-    private final ListView<T> rootListView = new ListView<>(){
-        @Override
-        protected double computePrefWidth(double width) {
-            return filterComboBox.getWidth();
-        }
-
-        @Override
-        protected double computePrefHeight(double height) {
-            double h = popupHeight == -1? filterComboBox.getHeight() : popupHeight;
-            return 5 + h * (Math.min(FilterComboBoxListViewSkin.this.filterComboBox.getItems().size() + 1, FilterComboBoxListViewSkin.this.filterComboBox.getVisibleRowCount()));
-        }
-    };
+    private VBox vBox;
 
     private final ListView<T> realListView;
 
+    private final ObservableList<T> rawItems;
+
     private boolean isOuter = true;
 
-    private double popupHeight = -1;
 
     @SuppressWarnings("all")
     public FilterComboBoxListViewSkin(FilterComboBox<T> control) {
         super(control);
-
         this.filterComboBox = control;
         this.filterField.setRealTime(true);
+        realListView = (ListView) super.getPopupContent();
+        vBox = new VBox(){
+            @Override
+            protected double computePrefWidth(double height) {
+                return realListView.prefWidth(height);
+            }
 
+            @Override
+            protected double computePrefHeight(double width) {
+                return realListView.prefHeight(width) + filterPane.prefHeight(width);
+            }
+        };
+        filterPane.setStyle("-fx-padding: 5");
+        vBox.getChildren().addAll(filterPane, realListView);
+        vBox.setStyle("-fx-background-color: white");
+        realListView.setManaged(true);
         if (control.getStyleClass().contains("combo-box-ui")){
             filterField.getStyleClass().add("text-field-ui");
             if (control.getStyleClass().contains("combo-box-ui-small")) {
@@ -69,83 +70,53 @@ public class FilterComboBoxListViewSkin<T> extends ComboBoxListViewSkin<T> {
                 filterField.getStyleClass().addAll("text-field-ui-small");
             }
         }
+        rawItems = FXCollections.observableArrayList(realListView.getItems());
 
-        realListView = (ListView) super.getPopupContent();
-        realListView.getItems().addListener((ListChangeListener<? super T>) change -> {
-            rootListView.getItems().remove(1, rootListView.getItems().size());
-            rootListView.getItems().addAll(realListView.getItems());
-        });
-
-        realListView.getSelectionModel().selectedItemProperty().addListener((observableValue, t, t1) -> {
-            for (Object item : rootListView.getItems()) {
-                if (Objects.equals(item, t1) && realListView.getSelectionModel().getSelectedIndex() != 0){
-                    rootListView.getSelectionModel().select(t1);
+        realListView.itemsProperty().addListener((observable, oldValue, newValue) -> {
+            rawItems.setAll(newValue);
+            newValue.addListener((ListChangeListener<? super T>) observable1 -> {
+                if (isOuter) {
+                    rawItems.setAll(realListView.getItems());
                 }
-            }
+            });
         });
-
-        rootListView.setCellFactory(new Callback<ListView<T>, ListCell<T>>() {
-            @Override
-            public ListCell<T> call(ListView<T> param) {
-                return new ListCell<>(){
-
-                    @Override
-                    protected void updateItem(T item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (!empty && getIndex() == 0 && item == null){
-                            setText("");
-                            setGraphic(filterPane);
-                            filterPane.setMaxWidth(filterComboBox.getWidth() - 20);
-                            setStyle("-fx-background-color: white!important;");
-                        }else {
-                            setGraphic(null);
-                            setText(getShowText(getItem()));
-                            setStyle("");
-                        }
-                    }
-
-                };
+        realListView.getItems().addListener((ListChangeListener<? super T>) observable -> {
+            if (isOuter) {
+                rawItems.setAll(realListView.getItems());
             }
         });
 
         filterField.setOnFilterAction(text -> {
             isOuter = false;
-            ObservableList<T> rootListViewItems = rootListView.getItems();
             if (text == null || text.isBlank()){
-                if (rootListViewItems.size() != realListView.getItems().size() + 1){
-                    T selectedItem = realListView.getSelectionModel().getSelectedItem();
-                    rootListViewItems.remove(1, rootListViewItems.size());
-                    rootListViewItems.addAll(realListView.getItems());
-                    rootListView.getSelectionModel().select(selectedItem);
-                }
-            }else {
+                System.out.println("raw");
+//                realListView.getItems().setAll(rawItems);
                 T selectedItem = realListView.getSelectionModel().getSelectedItem();
-                rootListViewItems.remove(1, rootListViewItems.size());
-                List<T> resullt = realListView.getItems()
+                List<T> resullt = rawItems
                         .stream()
-                        .filter(item -> getShowText(item).contains(text) || (control.isIgnoreCase() && getShowText(item).toLowerCase().contains(text.toLowerCase())))
+                        .filter(item -> !Objects.equals(selectedItem, item) && (getShowText(item).contains(text) || (control.isIgnoreCase() && getShowText(item).toLowerCase().contains(text.toLowerCase()))))
                         .toList();
-                rootListViewItems.addAll(resullt);
-                rootListView.getSelectionModel().select(selectedItem);
+                realListView.getItems().removeIf(item -> !Objects.equals(selectedItem, item));
+                realListView.getItems().addAll(resullt);
+            }else {
+                System.out.println("filter:" + text);
+                T selectedItem = realListView.getSelectionModel().getSelectedItem();
+                Set<T> resullt = rawItems
+                        .stream()
+                        .filter(item -> (getShowText(item).contains(text) || (control.isIgnoreCase() && getShowText(item).toLowerCase().contains(text.toLowerCase()))))
+                        .collect(Collectors.toSet());
+                if (resullt.isEmpty()) {
+                } else  if (resullt.contains(selectedItem)){
+                    List<T> list = resullt.stream().filter(item -> !Objects.equals(selectedItem, item)).toList();
+                    realListView.getItems().removeIf(item -> !Objects.equals(selectedItem, item));
+                    realListView.getItems().addAll(list);
+                }else {
+                    realListView.getItems().setAll(resullt);
+                }
             }
-            rootListView.refresh();
+
             isOuter = true;
         });
-
-        rootListView.getSelectionModel().selectedItemProperty().addListener((observableValue, o, t1) -> {
-            if (isOuter){
-                if (rootListView.getSelectionModel().getSelectedIndex() != 0){
-                    this.filterComboBox.hide();
-                    realListView.getSelectionModel().select(t1);
-                }
-            }
-        });
-        filterPane.prefWidthProperty().bind(control.widthProperty().subtract(5));
-//        防止搜索框所在item被选中
-        filterPane.addEventHandler(MouseEvent.MOUSE_PRESSED, Event::consume);
-        rootListView.getItems().add(null);
-        rootListView.getItems().addAll(realListView.getItems());
-        rootListView.getSelectionModel().select(filterComboBox.getValue());
     }
 
     @NotNull
@@ -160,22 +131,18 @@ public class FilterComboBoxListViewSkin<T> extends ComboBoxListViewSkin<T> {
     @Override
     public void show() {
         super.show();
-        double insect = 22D;
-        filterField.setMaxWidth(filterComboBox.getWidth() - insect);
-        filterField.setMinWidth(filterComboBox.getWidth() - insect);
-        filterField.requestFocus();
+        Platform.runLater(filterField::requestFocus);
     }
 
     @Override
-    protected double computePrefHeight(double width, double topInset, double rightInset, double bottomInset, double leftInset) {
-        popupHeight = super.computePrefHeight(width, topInset, rightInset, bottomInset, leftInset);
-        return super.computePrefHeight(width, topInset, rightInset, bottomInset, leftInset);
+    public void hide() {
+        super.hide();
+        filterField.setText("");
     }
 
     @Override
     public Node getPopupContent() {
-//        只能返回ListView类型，否则popup布局会异常
-        return rootListView;
+        return vBox;
     }
 
 }
