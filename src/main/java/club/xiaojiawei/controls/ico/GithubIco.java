3b2c35296d043034ebf08545dbb517d8ce6583b3
zergqueen <xjw580@qq.com>
2025-02-10 20:14:53 +0800

    v2.25.0
    1. 增加图标GiteeIco，GithubIco

---
+++ src/main/java/club/xiaojiawei/controls/ico/GithubIco.java
@@ -0,0 +1,26 @@
+package club.xiaojiawei.controls.ico;
+
+import club.xiaojiawei.controls.images.ImagesLoader;
+import org.girod.javafx.svgimage.SVGImage;
+import org.girod.javafx.svgimage.SVGLoader;
+
+/**
+ * @author 肖嘉威 xjw580@qq.com
+ * @date 2023/10/31 22:28
+ */
+public class GithubIco extends AbstractIco {
+
+    public GithubIco() {
+        this(null);
+    }
+
+    public GithubIco(String color) {
+        super(color);
+        SVGImage svgImage = SVGLoader.load(ImagesLoader.class.getResource(this.getClass().getSimpleName() + ".svg"));
+        if (svgImage != null) {
+            this.setMaxWidth(svgImage.getWidth());
+            this.getChildren().add(svgImage);
+        }
+    }
+
+}
